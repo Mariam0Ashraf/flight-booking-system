8dbe668790f02c49dcb95232cba984acef8eacc2
mariam0ashraf <mariam00ashraf@gmail.com>
2025-12-24 08:52:10 +0200

    change\

--- core/db_connect.php
+++ core/db_connect.php
@@ -2,7 +2,7 @@
 $host = 'localhost';
 $db   = 'flight_booking_db';
 $user = 'root';
-$pass = '#Magcds006052003';
+$pass = '';
 $charset = 'utf8mb4';
 $dsn = "mysql:host=$host;dbname=$db;charset=$charset";
 $options = [
